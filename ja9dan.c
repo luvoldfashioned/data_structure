c9b636cb6c7cd31ffc72fc5f791fb4fef6c36fb2
Seungbin Yeom <storm6781@naver.com>
2025-04-26 12:16:03 +0900

    update

--- ja9dan.c
+++ ja9dan.c
@@ -89,3 +89,159 @@ int delete(int arr[], int x)
 
     return arr;
 }
+
+// 자구단 #05 연결 리스트 선형 탐색
+node *node::search(data_type item)
+{
+    // 1. Find a proper position
+    node *curr = this; // node를 방문할 포인터 curr
+    while (curr != NULL)
+    {
+        if (curr->item == item)
+            return curr; // Found
+        curr = curr->link;
+    }
+
+    return NULL; // Not found
+}
+
+// 자구단 #06 단일 연결 리스트 삽입 O(n)
+void main()
+{
+    first.insert("GAT");
+}
+
+void hnode::insert(data_type item)
+{
+    this->link->insert(item);
+}
+
+void node::insert(data_type item) // GATD
+{
+    // 1. 적절한 위치를 결정
+    node *curr = this;
+    while (curr->link != NULL)
+    {
+        if (curr->link->item > item) // 다음 원소 item이 현재 내가 삽입하려는 것보다 클 때
+            break;                   // 이 링크가 FAT의 링크. curr의 link를 따짐 따라서 curr는 FAT curr->link는 HAT
+        curr = curr->link;
+    }
+
+    // 2. 새로운 노드를 생성
+    node *hnode = new node; // node크기만큼의 기억공간 할당
+    nnode->item = item;     // nnode에 GAT 할당
+
+    // 3. 링크를 갱신
+    nnode->link = curr->link; // nnode->link가 HAT여야 됨. HAT는 지금 curr->link임임 (오른쪽 화살표)
+    curr->link = nnode;       // 왼쪽 화살표
+}
+
+// 자구단 #07 단일 연결 리스트 삭제 O(n)
+void main()
+{
+    first.delete("FAT");
+}
+
+void hnode::delete(data_type item)
+{
+    this->link->delete(item);
+}
+
+void node::delete(data_type item)
+{
+    // 1. 삭제할 원소를 찾음 하나 앞
+    node *curr = this;
+    while (curr->link != NULL)
+    {
+        if (curr->link->item == item)
+            break;
+        curr = curr->link;
+    }
+    if (curr == NULL)
+        return;
+    // 2. 링크를 갱신
+    node *dnode = curr->link; // dnode: FAT dnode->link: HAT
+    curr->link = dnode->link; // curr->link: FAT 에서 dnode->link: HAT 갱신하기
+    // 3. 삭제된 node를 free
+    free(dnode);
+}
+
+// 자구단 #08 이준 연결 리스트 삽입 O(n)
+void node::insert(data_type item)
+{
+    // 1. 적절한 위치를 결정
+    node *curr = this;
+    while (curr->rlink != NULL)
+    {
+        if (curr->rlink->item > item)
+            break;
+        curr = curr->rlink;
+    }
+    // 2. 새로운 노드를 생성
+    node *nnode = new node;
+    nnode->item = item;
+    // 3. 링크를 갱신
+    // 3.1 rlink 방향 (rlink: ->)
+    nnode->rlink = curr->rlink;
+    curr->rlink = nnode;
+    // 3.2 llink 방향 (link: <-)
+    nnode->llink = curr;
+    nnode->rlink->llink = nnode;
+}
+
+// 자구단 #09 이중 연결 리스트 삭제 O(n)
+void node::delete(data_type item)
+{
+    // 1. 삭제할 원소를 찾음
+    node *curr = first;
+    while (curr->rlink != NULL)
+    {
+        if (curr->rlink->item == item)
+            break;
+        curr = curr->rlink;
+    }
+    // 2. 링크를 갱신
+    node *dnode = curr->rlink;
+    curr->rlink = dnode->rlink;
+    curr->rlink->llink = dnode->llink; // 아니면 curr
+    // 3. 삭제된 노드를 free
+    free(dnode);
+}
+
+// 자구단 #10 스택 push
+void Stack::push(Datatype DataItem)
+{
+    if (is_Full())
+        printf("Pushing in Full stack\n");
+
+    Items[TOP] = DataItem;
+    TOP++;
+}
+
+// 자구단 #11 스택 pop
+Datatype Stack::pop()
+{
+    if (is_Empty())
+        printf("Poping from Empty Stack\n");
+
+    TOP--;
+    retrun Items[TOP];
+}
+
+// 자구단 #12 큐 push
+void queue::push(element item)
+{
+    if (isFull())
+        printf("cannot add an element to a full queue");
+    
+    Items[++rear] = item;
+}
+
+// 자구단 #13 큐 pop
+void queue::pop(element item)
+{
+    if(isEmpty())
+        printf("You cannot delete from an empty queue");
+    
+    return Items[++front];
+}
